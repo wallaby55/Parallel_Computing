a98fff7b4f43ba4f8154f09653f74c7daa5b3b25
Andrew Pauls <91347926+wallaby55@users.noreply.github.com>
2023-03-21 15:52:13 -0400

    Add files via upload
    very similar program to main branch, however here I have incorporated locks so that we never reach deadlock.

--- Main.java
+++ Main.java
@@ -1,15 +1,20 @@
 package Philosophers;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
+
+
     public static void main(String[] args) {
+        ReentrantLock lock = new ReentrantLock();
         Thread[] threads = new Thread[5];
         Chopstick[] chopsticks = new Chopstick[5];
         for(int i = 0 ; i < chopsticks.length; i++){
             chopsticks[i] = new Chopstick(i);
         }
 
-
         for(int i = 0 ; i < threads.length; i++) {
-            threads[i] = new Thread(new Philosopher("Philosopher"+i, chopsticks));
+            threads[i] = new Thread(new Philosopher("Philosopher"+i, chopsticks, lock));
             threads[i].start();
         }
         System.out.println("First I will build this timer environment");
--- Philosopher.java
+++ Philosopher.java
@@ -1,6 +1,7 @@
 package Philosophers;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Philosopher implements Runnable {
 
@@ -13,11 +14,13 @@ public class Philosopher implements Runnable {
     int chopHeldInLeft;
     int chopHeldInRight;
     Chopstick[] sticks;
+    ReentrantLock lock;
 
 
-    public Philosopher(String name, Chopstick[] sticks) {
+    public Philosopher(String name, Chopstick[] sticks, ReentrantLock L) {
         this.name = name;
         this.sticks = sticks;
+        this.lock = L;
         leftHandHasChopstick = false;
         rightHandHasChopstick = false;
         chopHeldInLeft = -1;
@@ -26,7 +29,9 @@ public class Philosopher implements Runnable {
 
     private void leftHandPickedUp(int i) {
         // set the chopstick to a picked up state
+        lock.lock();
         sticks[i].pickUp();
+        lock.unlock();
         // set this philosophers left hand to being occupied
         this.leftHandHasChopstick = true;
         // acquire the ID of the chopstick
@@ -34,17 +39,22 @@ public class Philosopher implements Runnable {
     }
 
     private void leftHandPutDown(int i) {
+        lock.lock();
         // put the stick down
         sticks[i].putDown();
+        lock.unlock();
         // set the philosophers hand to being open
         this.leftHandHasChopstick = false;
         // reassign the value to -1 (represents no stick)
         chopHeldInLeft = -1;
+
     }
 
     private void rightHandPickedUp(int i) {
         // set the chopstick to a picked up state
+        lock.lock();
         sticks[i].pickUp();
+        lock.unlock();
         // set this philosophers left hand to being occupied
         this.rightHandHasChopstick = true;
         // acquire the ID of the chopstick
@@ -52,27 +62,34 @@ public class Philosopher implements Runnable {
     }
 
     private void rightHandPutDown(int i) {
+        lock.lock();
         // put the stick down
         sticks[i].putDown();
+        lock.unlock();
         // set the philosophers hand to being open
         this.rightHandHasChopstick = false;
         // reassign the value to -1 (represents no stick)
         chopHeldInRight = -1;
+
     }
 
 
     public void run() {
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 20000; i++) {
             try {
                 int delay = random.nextInt(50); // generate a random delay between 0 and 5000 milliseconds
                 TimeUnit.MILLISECONDS.sleep(delay); // sleep for the random delay
                 System.out.println(this.name + " entered iteration" + i);
+
                 eat();
+
                 System.out.println(name + " started eating after waiting for " + delay + " milliseconds.");
                 // then, some kind of delay...
                 int delay2 = random.nextInt(50); // generate a random delay between 0 and 5000 milliseconds
                 TimeUnit.MILLISECONDS.sleep(delay2); // sleep for the random delay
+                lock.lock();
                 think();    // put back the sticks
+                lock.unlock();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -84,8 +101,10 @@ public class Philosopher implements Runnable {
         while (!this.leftHandHasChopstick | !this.rightHandHasChopstick){
             for (int i = 0; i < sticks.length; i++) {
                 // scan for unheld stick
+                lock.lock();
                 if (sticks[i].chopAvailable()) {
                     sticks[i].pickUp();
+                    lock.unlock();
                     this.leftHandHasChopstick = true;
                     this.chopHeldInLeft = sticks[i].getChopID();
                     System.out.println(this.name + " picked up " + sticks[i].getChopID() + " in their left hand");
@@ -108,12 +127,16 @@ public class Philosopher implements Runnable {
     private void think(){
         // put down the stick in the left hand
         this.leftHandHasChopstick = false;
+        lock.lock();
         sticks[this.chopHeldInLeft].putDown();
+        lock.unlock();
         this.chopHeldInLeft = -1;
 
         // put down the stick in the right hand
         this.rightHandHasChopstick = false;
+        lock.lock();
         sticks[this.chopHeldInRight].putDown();
+        lock.unlock();
         this.chopHeldInRight = -1;
 
         System.out.println(this.name + " finished eating and put down both sticks to think");
